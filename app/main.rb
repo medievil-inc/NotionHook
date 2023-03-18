e4751b853c83e40ec8b329d3eeca06ebbef53a8c
Ivan Kudinov <medievil.inc@gmail.com>
2023-03-18 22:54:23 +0300

    6b7589b78374464b9fca58e52adee36e

--- app/main.rb
+++ app/main.rb
@@ -1,1 +1,11 @@
-puts 'Hello world'
+require 'octokit'
+
+github_token = ENV['PERSONAL_ACCESS_TOKEN']
+notion_secret = ENV['NOTION_SECRET']
+
+puts "#{github_token}: #{notion_secret}"
+
+client = Octokit::Client.new(:access_token => github_token)
+
+user = client.user
+user.login
