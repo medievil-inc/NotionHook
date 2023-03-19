1cc1248adf1283b2d5f4c417f8e888a639ecec18
Ivan Kudinov <medievil.inc@gmail.com>
2023-03-19 10:49:58 +0300

    Update main.rb

--- app/main.rb
+++ app/main.rb
@@ -3,6 +3,17 @@ require 'octokit'
 github_token = ENV['PERSONAL_ACCESS_TOKEN']
 notion_secret = ENV['NOTION_SECRET']
 
+puts 'STARTING'
+puts ENV["GITHUB_EVENT_NAME"]
+puts ENV["GITHUB_SHA"]
+puts ENV["GITHUB_REF"]
+puts ENV["GITHUB_WORKFLOW"]
+puts ENV["GITHUB_ACTION"]
+puts ENV["GITHUB_ACTOR"]
+puts ENV["GITHUB_JOB"]
+puts 'ENDING'
+
+
 client = Octokit::Client.new(:access_token => github_token)
 
 user = client.user
