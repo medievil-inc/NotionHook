083f37958dc45a1bd0a12f577cb97df21b62ea56
Ivan Kudinov <medievil.inc@gmail.com>
2023-03-23 16:00:56 +0300

    6b7589b78374464b9fca58e52adee36e

--- app/main.rb
+++ app/main.rb
@@ -12,99 +12,23 @@ require_relative './utils/notion_helpers'
 # 🐣 Author: Ivan Kudinov
 # 📫 URL: https://github.com/medievil-inc/notion-hook/commit/64700475ec193055ce0a3c88524b50ae53c2c337
 
-begin
-    notion_secret = ENV.fetch('NOTION_SECRET', nil)
+notion_secret = ENV.fetch('NOTION_SECRET', nil)
 
-    notion = Notion::Client.new(token: notion_secret)
+notion = Notion::Client.new(token: notion_secret)
 
-    payload = GitHubEventProcessor.load_payload
+payload = GitHubEventProcessor.load_payload
 
-    pull_request = payload['pull_request']
-    commits = payload['commits']
+pull_request = payload['pull_request']
+commits = payload['commits']
 
-    puts '------------------------------------------'
-    puts JSON.pretty_generate(payload)
-    puts '------------------------------------------'
+puts '------------------------------------------'
+puts JSON.pretty_generate(payload)
+puts '------------------------------------------'
 
-    if pull_request
-        create_comment_by_pr(notion, payload, pull_request)
-    elsif commits.length.positive?
-        commits.each { |commit| create_comment_by_commit(notion, payload, commit) }
-    else
-        create_comment_by_commit(notion, payload, payload['head_commit'])
-    end
-rescue StandardError => e
-    puts e.message
-    exit 1
-end
-
-def create_comment_by_commit(notion, payload, commit)
-    page = NotionHelpers.search_page(notion, commit)
-
-    return if page.nil? || page == 'undefined'
-
-    notion.create_comment(
-        {
-            parent: {
-                page_id: page.id
-            },
-            rich_text: [
-                create_comment_title(content: '💬 Commit: '),
-                create_comment_description(content: "#{commit.message.gsub(/(\r\n|\n|\r)/, ' ')}\n"),
-                create_comment_title(content: '👀 Branch: '),
-                create_comment_description(
-                    content: "#{payload.ref}\n",
-                    color: 'purple'
-                ),
-                create_comment_title(content: '🐣 Author: '),
-                create_comment_description(
-                    content: "#{commit.author.name}\n",
-                    color: 'yellow',
-                    code: true
-                ),
-                create_comment_title(content: '📫 URL: '),
-                create_comment_description(
-                    content: commit.url.to_s,
-                    color: 'blue',
-                    url: commit.url
-                )
-            ]
-        }
-    )
-    .then do |result|
-        JSON.pretty_generate(result)
-    .catch do |e|
-        puts e.message
-        exit 1
-    end
-end
-
-def create_comment_by_pr(notion, payload, pull_request)
-    # your code here
-end
-
-def create_comment_title(content, color: 'gray')
-    {
-        text: {
-            content:
-        },
-        annotations: {
-            color:
-        }
-    }
-end
-
-def create_comment_description(content, color, code: false, url: nil)
-    {
-        text: {
-            content:,
-            link: {
-                url:
-            }
-        },
-        annotations: {
-            code:,
-            color:
-        }
-    }
+if pull_request
+    NotionHelpers.create_comment_by_pr(notion, payload, pull_request)
+elsif commits.length > 1
+    commits.each { |commit| NotionHelpers.create_comment_by_commit(notion, payload, commit) }
+else
+    NotionHelpers.create_comment_by_commit(notion, payload, payload['head_commit'])
 end
--- app/utils/notion_helpers.rb
+++ app/utils/notion_helpers.rb
@@ -3,7 +3,79 @@
 # frozen_string_literal: true
 
 module NotionHelpers
-    def self.search_page(notion, commit)
+    module_function
+
+    def create_comment_by_commit(notion, payload, commit)
+        page = search_page(notion, commit)
+
+        return if page.nil?
+
+        options = {
+            parent: {
+                page_id: page.id
+            },
+            rich_text: [
+                create_comment_title(content: '💬 Commit: '),
+                create_comment_description(content: "#{commit.message.gsub(/(\r\n|\n|\r)/, ' ')}\n"),
+                create_comment_title(content: '👀 Branch: '),
+                create_comment_description(
+                    content: "#{payload.ref}\n",
+                    color: 'purple'
+                ),
+                create_comment_title(content: '🐣 Author: '),
+                create_comment_description(
+                    content: "#{commit.author.name}\n",
+                    color: 'yellow',
+                    code: true
+                ),
+                create_comment_title(content: '📫 URL: '),
+                create_comment_description(
+                    content: commit.url.to_s,
+                    color: 'blue',
+                    url: commit.url
+                )
+            ]
+        }
+
+        notion.create_comment(options) do |result|
+            puts JSON.pretty_generate(result)
+        end
+    rescue StandardError => e
+        puts e.message
+        exit 1
+    end
+
+    def create_comment_by_pr(notion, payload, pull_request)
+        # your code here
+    end
+
+    def create_comment_title(content, color: 'gray')
+        {
+            text: {
+                content:
+            },
+            annotations: {
+                color:
+            }
+        }
+    end
+
+    def create_comment_description(content, color, code: false, url: nil)
+        {
+            text: {
+                content:,
+                link: {
+                    url:
+                }
+            },
+            annotations: {
+                code:,
+                color:
+            }
+        }
+    end
+
+    def search_page(notion, commit)
         regex = '(?=(?:.*?[A-Za-z]))(?=(?:.*?[0-9]))[A-Za-z0-9]{32}'
         query = commit.message.match(regex)&.[](0)
 
